e890081c150e5df51b7dd65745789808bdcaf0ff
Tuomas Paasonen <tuomas.paasonen@futurice.com>
2018-06-08 13:22:28 +0300

    Setting up

---
+++ modules/db.js
@@ -0,0 +1,11 @@
+var initKnex = require('knex');
+
+var knex = initKnex({
+  client: 'mysql',
+  connection: {
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME
+  }
+});
